5f7e5e3df2e2b0b15d49ecde49a07138c5bd4866
nic803 <nic@tmklive.com>
2026-03-26 18:05:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -872,7 +872,6 @@ teacher = _bundle_part(bundle, "teacher", "teacher_key")
 
 selected_products = tuple(_field(selection, "selected_products", default=()))
 recap_products = tuple(_field(selection, "recap_products", default=()))
-    selection_reasons = tuple(_field(selection, "selection_reasons", default=()))
     vocab_supported = tuple(_field(selection, "vocab_supported", default=()))
     structural_tags = tuple(_field(selection, "structural_tags", default=()))
 
