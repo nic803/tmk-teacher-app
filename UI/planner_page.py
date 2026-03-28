3d1a59aae67672da73c56f05567c46c12f001791
nic803 <nic@tmklive.com>
2026-03-28 19:13:42 +0700

    Update planner_page.py

--- UI/planner_page.py
+++ UI/planner_page.py
@@ -3,7 +3,7 @@ import streamlit as st
 from ui.components import (
     page_header,
     render_cumulative_product_map,
-    render_stage_summary
+    render_stage_summary,
 )
 
 
@@ -14,17 +14,24 @@ def render_structural_planner_page(view_model):
     No TMK logic allowed here.
     """
 
+    title = view_model.get("title", "")
+    subtitle = view_model.get("subtitle", "")
+    cumulative_map = view_model.get("cumulative_map", {})
+    selected_stage = view_model.get("selected_stage")
+    active_product = view_model.get("active_product")
+    stage_summary = view_model.get("stage_summary", {})
+
     page_header(
-        view_model["title"],
-        view_model["subtitle"]
+        title,
+        subtitle,
     )
 
     render_cumulative_product_map(
-        stage_products=view_model["cumulative_map"],
-        active_stage=view_model["selected_stage"],
-        active_product=view_model.get("active_product")
+        stage_products=cumulative_map,
+        active_stage=selected_stage,
+        active_product=active_product,
     )
 
     render_stage_summary(
-        view_model["stage_summary"]
+        stage_summary,
     )
